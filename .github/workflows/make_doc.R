105818ac0f3da8785f226c5b9f2c27d40d513f61
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2024-02-22 17:16:35 +0100

    Update make_doc.R

--- .github/workflows/make_doc.R
+++ .github/workflows/make_doc.R
@@ -4,6 +4,8 @@ if (length(pkgs_to_install)>0) install.packages(pkgs_to_install)
 
 devtools::install_github("eurostat/restatapi")
 cat(getwd())
+cat(Sys.info()['release'])
 message(getwd())
+print(Sys.info()['release'])
 pkgdown::build_site()
 system("git config --global --add safe.directory /__w/restatapi/restatapi")
