50dc80c1100d836222282fd64e5c4aec407628ac
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2024-02-22 09:22:42 +0100

    Update make_doc.R

--- .github/workflows/make_doc.R
+++ .github/workflows/make_doc.R
@@ -1,5 +1,10 @@
 if (!("pkgdown" %in% rownames(installed.packages()))){install.packages("pkgdown")}
 if (!("devtools" %in% rownames(installed.packages()))){install.packages("devtools")}
+if (!("data.table" %in% rownames(installed.packages()))){install.packages("data.table")}
+if (!("rjson" %in% rownames(installed.packages()))){install.packages("rjson")}
+if (!("xml2" %in% rownames(installed.packages()))){install.packages("xml2")}
+
+
 devtools::install_github("eurostat/restatapi")
 cat(getwd())
 message(getwd())
